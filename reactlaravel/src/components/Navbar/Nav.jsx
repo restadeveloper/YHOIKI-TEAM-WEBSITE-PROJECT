48426af46a80c8f2d0805f95292fb87591ccc012
resta graphic <bullreson51@gmail.com>
2024-08-29 21:36:03 +0700

    navbar rapi

--- reactlaravel/src/components/Navbar/Nav.jsx
+++ reactlaravel/src/components/Navbar/Nav.jsx
@@ -1,24 +1,24 @@
 import { Search, ChevronDown } from "react-feather";
-import logo from "../../assets/img/logo.png";
+import logo from "../../assets/img/logotop.png"
 import "./Nav.css"
 
-const Nav = () => {
+const Navbar_buatan_resta = () => {
     return (
         <>
             <div className="navbar">
                 <div className="background">
                     <div className="wadah_navbar">
-                        <div className="logonya"><img src={logo} alt="y" /><a href="">Yhoiki Team</a></div>
+                        <div className="logonya"><img src={logo} alt="y" /><p className="text_logo">Yhoiki Team</p></div>
                         <ul className="subisi">
                             <li><a href="Need Dev">Need Dev</a></li>
-                            <li className="adacevron"><a href="Order">Order</a><ChevronDown className="ChevronDown" /></li>
-                            <li className="adacevron"><a href="Need Dev">Bootcamp</a><ChevronDown className="ChevronDown" /></li>
-                            <li className="adacevron"><a href="Need Dev">Comunity</a><ChevronDown className="ChevronDown" /></li>
+                            <li className="adacevron"><a href="Order">Order</a><ChevronDown className="ChevronDown"/></li>
+                            <li className="adacevron"><a href="Need Dev">Bootcamp</a><ChevronDown className="ChevronDown"/></li>
+                            <li className="adacevron"><a href="Need Dev">Comunity</a><ChevronDown className="ChevronDown"/></li>
                         </ul>
                         <ul className="LogTer">
-                            <Search />
-                            <li><a href="Need Dev">Log In</a></li>
-                            <li><a href="Need Dev">Register</a></li>
+                            <Search className="search"/>
+                            <li className="login"><a href="Need Dev">Log In</a></li>
+                            <li className="register"><a href="Need Dev">Sign In</a></li>
                         </ul>
                     </div>
                 </div>
@@ -27,4 +27,4 @@ const Nav = () => {
     )
 }
 
-export default Nav;
+export default Navbar_buatan_resta;
