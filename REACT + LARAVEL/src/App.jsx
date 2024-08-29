cec07dd05ed59327037e1455c6fb7208897e8e39
restadeveloper <restanugroho21@gmail.com>
2024-08-29 14:47:43 +0700

    Add files via upload

---
+++ REACT + LARAVEL/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Nav from './Navbar/Nav'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Nav className="Navbarcuy"/>
+      <h1>
+        ini bagian depan
+      </h1>
+    </>
+  )
+}
+
+export default App
