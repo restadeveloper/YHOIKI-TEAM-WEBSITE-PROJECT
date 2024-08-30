6e5f95c804af2c60daf336312fbdc9595b87b76e
resta graphic <bullreson51@gmail.com>
2024-08-30 07:40:10 +0700

    aku ganti navnya

--- reactlaravel/src/App.jsx
+++ reactlaravel/src/App.jsx
@@ -1,14 +1,20 @@
 
 import Nav from "./components/Navbar/Nav";
-import './App.css'
-// import { LogIn } from 'react-feather';
 import Login from "./components/Login/Login";
+import Vscode from "./assets/img/capture.png"
+import './App.css'
 
 
 const App = () => {
   return (
     <>
-      <Nav />
+      <div className="navbarnya">
+      <Nav className='Nav'/>
+      </div>
+      <h1>
+        Mau Cari Atau Ingin Jadi Developer?
+      </h1>
+      <img src={Vscode} className="Vscode"/>
     </>
   )
 }
--- reactlaravel/src/components/Navbar/Nav.jsx
+++ reactlaravel/src/components/Navbar/Nav.jsx
@@ -1,4 +1,5 @@
-import { Search, ChevronDown } from "react-feather";
+import { IoSearchSharp } from "react-icons/io5";
+import { FaAngleDown } from "react-icons/fa"
 import logo from "../../assets/img/logotop.png"
 import "./Nav.css"
 
@@ -10,16 +11,16 @@ const Navbar_buatan_resta = () => {
                     <div className="wadah_navbar">
                         <div className="logonya"><img src={logo} alt="y" /><p className="text_logo">Yhoiki Team</p></div>
                         <ul className="subisi">
-                            <li><a href="Need Dev">Need Dev</a></li>
-                            <li className="adacevron"><a href="Order">Order</a><ChevronDown className="ChevronDown"/></li>
-                            <li className="adacevron"><a href="Need Dev">Bootcamp</a><ChevronDown className="ChevronDown"/></li>
-                            <li className="adacevron"><a href="Need Dev">Comunity</a><ChevronDown className="ChevronDown"/></li>
+                            <li className="adacevron"><a className='a' href="Need Dev">Need Dev</a></li>
+                            <li className="adacevron"><a className='a' href="Order">Order</a><FaAngleDown className="ChevronDown"/></li>
+                            <li className="adacevron"><a className='a' href="Need Dev">Bootcamp</a><FaAngleDown className="ChevronDown"/></li>
+                            <li className="adacevron"><a className='a' href="Need Dev">Comunity</a><FaAngleDown className="ChevronDown"/></li>
                         </ul>
-                        <ul className="LogTer">
-                            <Search className="search"/>
-                            <li className="login"><a href="Need Dev">Log In</a></li>
-                            <li className="register"><a href="Need Dev">Sign In</a></li>
-                        </ul>
+                        <div className="LogTer">
+                            <IoSearchSharp className="search"/>
+                            <a href="" className="login">Log In</a>
+                            <a href="" className="register">Sign In</a>
+                        </div>
                     </div>
                 </div>
             </div>
